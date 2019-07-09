7450a6b87aeb13af02d327b4a130128bb4587386
pbohora <pradeepbohora26@gmail.com>
2019-07-09 11:50:20 +0300

    add user feedback statstics

---
+++ Part 1/unicafe/src/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positivePercentage = (good / total) * 100;
+
+  const handleGoodClick = () => setGood(good + 1);
+
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+
+  const handleBadClick = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {total}</p>
+      <p>Average {average}</p>
+      <p>Positive {positivePercentage}%</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
