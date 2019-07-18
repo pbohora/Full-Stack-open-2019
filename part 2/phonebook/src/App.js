fb51f3ba506921dec94b0b329e0118d4c5796009
pbohora <pradeepbohora26@gmail.com>
2019-07-18 15:51:32 +0300

    added search filter and prevent adding duplicate contacts

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,53 +1,85 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("name");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "04012333" },
+    { name: "hari na", number: "04012333" },
+    { name: "kaisa lappa", number: "04012333" },
+    { name: "reej", number: "04012333" },
+    { name: "dillu", number: "04012333" }
+  ]);
+  const [newName, setNewName] = useState(" ");
+  const [newNumber, setNewNumber] = useState(" ");
+  const [serachName, setSearchName] = useState(" ");
 
-  const personDisplay = () =>
-    persons.map(person => <p key={person.name}>{person.name}</p>);
+  const person = () =>
+    persons
+      .filter(
+        person =>
+          serachName === "" ||
+          person.name.toLowerCase().includes(serachName.trim())
+      )
+      .map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ));
 
   const handleNameChange = e => {
-    console.log(e.target.value);
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = e => {
+    setNewNumber(e.target.value);
+  };
+
+  const handleSearchChange = e => {
+    setSearchName(e.target.value);
   };
 
   const addName = e => {
     e.preventDefault();
-    const personObj = { name: newName };
+    const personObj = { name: newName, number: newNumber };
 
     persons.map(person => {
-      if (person.name.indexOf(personObj.name) === -1) {
-        setPersons(persons.concat(personObj));
-        console.log(person);
+      if (
+        person.name
+          .trim()
+          .toLowerCase()
+          .indexOf(personObj.name.trim().toLowerCase()) === -1
+      ) {
+        return setPersons(persons.concat(personObj));
       } else {
-        console.log(`${newName} alerady exists!!`);
-        const persona = persons.filter(
-          (person, index) => persons.indexOf(person) === index
-        );
-        setPersons(persona);
+        setPersons(persons);
+        alert(`${newName} is already added to phonebook!!`);
       }
     });
 
-    console.log(persons);
-
     setNewName(" ");
+    setNewNumber(" ");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter with <input value={serachName} onChange={handleSearchChange} />
+      </div>
+      <h2>add new contact</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {personDisplay()}
-      <div>debug: {newName}</div>
+      {person()}
+      <div>debug: {serachName}</div>
     </div>
   );
 };
