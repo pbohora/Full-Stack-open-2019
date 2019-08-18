b059b93b9f3919ecf504bebf28d837d1fca19b12
pbohora <pradeepbohora26@gmail.com>
2019-08-18 22:33:09 +0300

    refactor into separate modules

---
+++ part4/bloglist/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+
+mongoose
+  .connect(config.mongoUrl, { useNewUrlParser: true })
+  .then(result => console.log("connected to the database"))
+  .catch(error => console.log("error connecting to mongoDB: ", error.message));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
---
+++ part4/bloglist/utils/config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+let mongoUrl = process.env.MONGODB_URI;
+
+let PORT = process.env.PORT;
+
+module.exports = {
+  mongoUrl,
+  PORT
+};
