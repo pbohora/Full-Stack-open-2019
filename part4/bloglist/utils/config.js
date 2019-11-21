5db09f31b672a716996de2229a1e78be30e9d007
pbohora <pradeepbohora26@gmail.com>
2019-11-21 20:29:53 +0200

    added dummy tests for helper functions

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 
 let mongoUrl = process.env.MONGODB_URI;
 
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
+
 let PORT = process.env.PORT;
 
 module.exports = {
